9c2b05681b5f58e1d3d3e167e6c428e554af91b7
tkjode <tkjode@gmail.com>
2024-09-19 01:07:24 -0400

    doc(talos): Steps for getting a cluster up with talosctl

--- provider/talos/README.md
+++ provider/talos/README.md
@@ -18,3 +18,31 @@ Steps (as I understand them):
 ## Tools
 
 Not sure yet.  Simple Ansible Playbooks may work well here to coordinate all required activities.  Or, might use a pipeline.  Haven't decided.
+
+## Commands Used
+
+```bash
+  140  curl -sL https://talos.dev/install | sh                      # installs TalosCTL
+  141  talosctl                                                     # make sure its in PATH
+  237  export TALOSCONFIG="_out/talosconfig"                        # Ideally set this to a static home path not cwd
+  238  talosctl config endpoint $CONTROL_PLANE_IP                   # I don't think this worked up front
+  239  talosctl config node $CONTROL_PLANE_IP                       # Same
+  240  talosctl gen config talon https://$CONTROL_PLANE_IP:6443 --output-dir _out        # generated conf sets for cplane+worker yamls
+  241  talosctl apply-config --insecure --nodes $CONTROL_PLANE_IP --file _out/controlplane.yaml     # Pushes conf file - but had problems with scsi disk
+  242  talosctl disks --insecure --nodes $CONTROL_PLANE_IP   # No SCSI
+  246  talosctl disks --insecure --nodes $CONTROL_PLANE_IP   # Switched to SATA, all good now
+  247  talosctl apply-config --insecure --nodes $CONTROL_PLANE_IP --file _out/controlplane.yaml   # First master configured
+  249  talosctl apply-config --insecure --nodes $CONTROL_PLANE_IP --file _out/controlplane.yaml   # Second master configured
+  251  talosctl apply-config --insecure --nodes $CONTROL_PLANE_IP --file _out/controlplane.yaml   # Third master configured
+  253  talosctl apply-config --insecure --nodes $WORKER_IP --file _out/worker.yaml                # First worker configured 
+  256  export WORKER_IP=10.0.0.181 && talosctl apply-config --insecure --nodes $WORKER_IP --file _out/worker.yaml # Streamlined single cmd, walked each worker
+  257  export WORKER_IP=10.0.0.182 && talosctl apply-config --insecure --nodes $WORKER_IP --file _out/worker.yaml #...
+  258  export WORKER_IP=10.0.0.183 && talosctl apply-config --insecure --nodes $WORKER_IP --file _out/worker.yaml #...
+  259  export WORKER_IP=10.0.0.184 && talosctl apply-config --insecure --nodes $WORKER_IP --file _out/worker.yaml #...
+  260  export WORKER_IP=10.0.0.185 && talosctl apply-config --insecure --nodes $WORKER_IP --file _out/worker.yaml #...
+  261  export TALOSCONFIG="_out/talosconfig"   # Superfluous
+  262  talosctl config endpoint 10.0.0.166   # Now this worked as it could reach a working master node
+  263  talosctl config node 10.0.0.166       # Same
+  264  talosctl bootstrap                # Fires up etcd and really gets things cooking
+  265  talosctl kubeconfig .             # Grab the admin kubeconfig, chuck it in ~/.kube/config and kubectl works.
+```
--- provider/terraform/proxmox_playground/main.tf
+++ provider/terraform/proxmox_playground/main.tf
@@ -9,9 +9,380 @@ terraform {
 
 provider "proxmox" {
   pm_api_url = "https://10.0.0.4:8006/api2/json"
+  pm_parallel=1
 }
 
 resource "proxmox_pool" "talon-kubernetes" {
   poolid = "talon-kubernetes"
   comment = "TalosVM-based Kubernetes Cluster"
+}
+
+resource "proxmox_vm_qemu" "talon-master-0" {
+  name = "talon-master-0"
+  desc = "One of Three Master Control Plane Nodes"
+  memory = 4096
+  sockets = 1
+  cores = 4
+  cpu = "x86-64-v2-AES"
+  pool = "talon-kubernetes"
+  tags = "control-plane"
+
+  target_node="proxmox"
+
+  network {
+    model="virtio"
+    macaddr="BC:24:11:04:01:01"
+    bridge="vmbr0"
+  }
+
+  disks {
+    ide {
+      ide0 {
+        cdrom {
+          iso="local:iso/metal-amd64.iso"
+        }
+      }
+    }
+
+    sata {
+      sata0 {
+        disk {
+          size="40G"
+          storage="SSD"
+          emulatessd=true
+          cache="writeback"
+          asyncio="threads"
+        }
+      }
+    }
+  }
+}
+
+resource "proxmox_vm_qemu" "talon-master-1" {
+  name = "talon-master-1"
+  desc = "Two of Three Master Control Plane Nodes"
+  memory = 4096
+  sockets = 1
+  cores = 4
+  cpu = "x86-64-v2-AES"
+  pool = "talon-kubernetes"
+  tags = "control-plane"
+
+  target_node="proxmox"
+
+  network {
+    model="virtio"
+    macaddr="BC:24:11:04:01:02"
+    bridge="vmbr0"
+  }
+
+  disks {
+    ide {
+      ide0 {
+        cdrom {
+          iso="local:iso/metal-amd64.iso"
+        }
+      }
+    }
+
+    sata {
+      sata0 {
+        disk {
+          size="40G"
+          storage="SSD"
+          emulatessd=true
+          cache="writeback"
+          asyncio="threads"
+        }
+      }
+    }
+  }
+}
+
+resource "proxmox_vm_qemu" "talon-master-2" {
+  name = "talon-master-2"
+  desc = "Three of Three Master Control Plane Nodes"
+  memory = 4096
+  sockets = 1
+  cores = 4
+  cpu = "x86-64-v2-AES"
+  pool = "talon-kubernetes"
+  tags = "control-plane"
+
+  target_node="proxmox"
+
+  network {
+    model="virtio"
+    macaddr="BC:24:11:04:01:03"
+    bridge="vmbr0"
+  }
+
+  disks {
+    ide {
+      ide0 {
+        cdrom {
+          iso="local:iso/metal-amd64.iso"
+        }
+      }
+    }
+
+    sata {
+      sata0 {
+        disk {
+          size="40G"
+          storage="SSD"
+          emulatessd=true
+          cache="writeback"
+          asyncio="threads"
+        }
+      }
+    }
+  }
+}
+
+resource "proxmox_vm_qemu" "talon-worker-0" {
+  name = "talon-worker-0"
+  desc = "A typical worker bee"
+  memory = 16384
+  sockets = 1
+  cores = 8
+  cpu = "x86-64-v2-AES"
+  pool = "talon-kubernetes"
+  tags = "worker"
+
+  target_node="proxmox"
+
+  network {
+    model="virtio"
+    macaddr="BC:24:11:04:01:10"
+    bridge="vmbr0"
+  }
+
+  disks {
+    ide {
+      ide0 {
+        cdrom {
+          iso="local:iso/metal-amd64.iso"
+        }
+      }
+    }
+
+    sata {
+      sata0 {
+        disk {
+          size="120G"
+          storage="SSD"
+          emulatessd=true
+          cache="writeback"
+          asyncio="threads"
+        }
+      }
+    }
+  }
+}
+
+resource "proxmox_vm_qemu" "talon-worker-1" {
+  name = "talon-worker-1"
+  desc = "A typical worker bee"
+  memory = 16384
+  sockets = 1
+  cores = 8
+  cpu = "x86-64-v2-AES"
+  pool = "talon-kubernetes"
+  tags = "worker"
+
+  target_node="proxmox"
+
+  network {
+    model="virtio"
+    macaddr="BC:24:11:04:01:11"
+    bridge="vmbr0"
+  }
+
+  disks {
+    ide {
+      ide0 {
+        cdrom {
+          iso="local:iso/metal-amd64.iso"
+        }
+      }
+    }
+
+    sata {
+      sata0 {
+        disk {
+          size="120G"
+          storage="SSD"
+          emulatessd=true
+          cache="writeback"
+          asyncio="threads"
+        }
+      }
+    }
+  }
+}
+
+resource "proxmox_vm_qemu" "talon-worker-2" {
+  name = "talon-worker-2"
+  desc = "A typical worker bee"
+  memory = 16384
+  sockets = 1
+  cores = 8
+  cpu = "x86-64-v2-AES"
+  pool = "talon-kubernetes"
+  tags = "worker"
+
+  target_node="proxmox"
+
+  network {
+    model="virtio"
+    macaddr="BC:24:11:04:01:12"
+    bridge="vmbr0"
+  }
+
+  disks {
+    ide {
+      ide0 {
+        cdrom {
+          iso="local:iso/metal-amd64.iso"
+        }
+      }
+    }
+
+    sata {
+      sata0 {
+        disk {
+          size="120G"
+          storage="SSD"
+          emulatessd=true
+          cache="writeback"
+          asyncio="threads"
+        }
+      }
+    }
+  }
+}
+
+resource "proxmox_vm_qemu" "talon-worker-3" {
+  name = "talon-worker-3"
+  desc = "A typical worker bee"
+  memory = 16384
+  sockets = 1
+  cores = 8
+  cpu = "x86-64-v2-AES"
+  pool = "talon-kubernetes"
+  tags = "worker"
+
+  target_node="proxmox"
+
+  network {
+    model="virtio"
+    macaddr="BC:24:11:04:01:13"
+    bridge="vmbr0"
+  }
+
+  disks {
+    ide {
+      ide0 {
+        cdrom {
+          iso="local:iso/metal-amd64.iso"
+        }
+      }
+    }
+
+    sata {
+      sata0 {
+        disk {
+          size="120G"
+          storage="SSD"
+          emulatessd=true
+          cache="writeback"
+          asyncio="threads"
+        }
+      }
+    }
+  }
+}
+
+resource "proxmox_vm_qemu" "talon-worker-4" {
+  name = "talon-worker-4"
+  desc = "A typical worker bee"
+  memory = 16384
+  sockets = 1
+  cores = 8
+  cpu = "x86-64-v2-AES"
+  pool = "talon-kubernetes"
+  tags = "worker"
+
+  target_node="proxmox"
+
+  network {
+    model="virtio"
+    macaddr="BC:24:11:04:01:14"
+    bridge="vmbr0"
+  }
+
+  disks {
+    ide {
+      ide0 {
+        cdrom {
+          iso="local:iso/metal-amd64.iso"
+        }
+      }
+    }
+
+    sata {
+      sata0 {
+        disk {
+          size="120G"
+          storage="SSD"
+          emulatessd=true
+          cache="writeback"
+          asyncio="threads"
+        }
+      }
+    }
+  }
+}
+
+resource "proxmox_vm_qemu" "talon-worker-5" {
+  name = "talon-worker-5"
+  desc = "A typical worker bee"
+  memory = 16384
+  sockets = 1
+  cores = 8
+  cpu = "x86-64-v2-AES"
+  pool = "talon-kubernetes"
+  tags = "worker"
+
+  target_node="proxmox"
+
+  network {
+    model="virtio"
+    macaddr="BC:24:11:04:01:15"
+    bridge="vmbr0"
+
+  }
+
+  disks {
+    ide {
+      ide0 {
+        cdrom {
+          iso="local:iso/metal-amd64.iso"
+        }
+      }
+    }
+
+    sata {
+      sata0 {
+        disk {
+          size="120G"
+          storage="SSD"
+          emulatessd=true
+          cache="writeback"
+          asyncio="threads"
+        }
+      }
+    }
+  }
 }
